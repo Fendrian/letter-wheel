da3fe144d679c6713dff41655c1794722416b8d5
Wolf Hatch <wolf@chaos.net.nz>
2017-05-27 13:57:26 +1200

    Mocking

--- src/containers/NewScreen.js
+++ src/containers/NewScreen.js
@@ -1,17 +1,45 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Alert, Button, Text, View } from 'react-native';
 
 import WrapperStyle from '../styles/WrapperStyle';
 import NewScreenStyle from '../styles/NewScreenStyle';
 
 export default class LoadingScreen extends React.Component {
+  start() {
+    Alert.alert('Coming soon', 'This feature is still under development.');
+    console.log('start game');
+  }
+  instructions() {
+    Alert.alert('Coming soon', 'This feature is still under development.');
+  }
   render() {
+    const { start, instructions } = this;
     return (
       <View style={WrapperStyle.container}>
-        <View style={NewScreenStyle.wrapper}>
-          <Text>
-            WELCOME!
+        <View style={NewScreenStyle.header}>
+          <Text style={NewScreenStyle.headerText}>
+            NEW GAME
           </Text>
+        </View>
+        <View style={NewScreenStyle.body}>
+          <View style={NewScreenStyle.words}>
+            <Text>
+              WORD COUNT
+            </Text>
+          </View>
+          <View style={NewScreenStyle.timer}>
+            <Text>
+              TIMER?
+            </Text>
+          </View>
+          <View style={NewScreenStyle.buttonWrapper}>
+            <View style={NewScreenStyle.buttons}>
+              <Button onPress={instructions} title={'Instructions'} />
+            </View>
+            <View style={NewScreenStyle.buttons}>
+              <Button onPress={start} title={'Start Game'} />
+            </View>
+          </View>
         </View>
       </View>
     );
--- src/styles/NewScreenStyle.js
+++ src/styles/NewScreenStyle.js
@@ -1,5 +1,46 @@
 import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
-  wrapper: {},
+  header: {
+    alignItems: 'center',
+    height: 70,
+    margin: 10,
+    width: 350,
+  },
+  body: {
+    alignItems: 'center',
+    flex: 1,
+    backgroundColor: '#fd6',
+    borderRadius: 4,
+    width: 350,
+  },
+  headerText: {
+    color: '#000',
+    fontSize: 50,
+    textAlign: 'center',
+  },
+  words: {
+    backgroundColor: '#a1d',
+    borderRadius: 4,
+    margin: 10,
+    width: 350,
+  },
+  timer: {
+    backgroundColor: '#a1d',
+    borderRadius: 4,
+    margin: 10,
+    width: 350,
+  },
+  buttonWrapper: {
+    flex: 1,
+    flexDirection: 'row',
+    borderRadius: 4,
+    margin: 10,
+    width: 350,
+  },
+  buttons: {
+    flex: 0.5,
+    margin: 10,
+    width: 350,
+  },
 });
