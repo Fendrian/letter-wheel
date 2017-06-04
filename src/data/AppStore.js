500ea2ee2531e7b4dfefb1498e605174bd10f9a3
Wolf Hatch <wolf@chaos.net.nz>
2017-06-05 10:45:08 +1200

    Javascript word generation is mostly working

--- src/data/AppStore.js
+++ src/data/AppStore.js
@@ -1,11 +1,11 @@
 import { observable } from 'mobx';
 import { Dimensions } from 'react-native';
-import dict4 from './dictionary_4';
-import dict5 from './dictionary_5';
-import dict6 from './dictionary_6';
-import dict7 from './dictionary_7';
-import dict8 from './dictionary_8';
-import dict9 from './dictionary_9';
+import dict4 from './dictionary_4.json';
+import dict5 from './dictionary_5.json';
+import dict6 from './dictionary_6.json';
+import dict7 from './dictionary_7.json';
+import dict8 from './dictionary_8.json';
+import dict9 from './dictionary_9.json';
 
 const shuffle = (string) => {
   const a = string.split('');
@@ -46,57 +46,82 @@ export default class AppState {
     },
   }
   newGame = (options) => {
-    // Generate a scrambled nine-letter word:
-    Object.assign(this.letters, (typeof (options.letters) === 'object') ? options.letters : (() => {
-      const word = dict9[Math.floor(Math.random() * dict9.length)];
-      const s = shuffle(word).split('');
-      return { 1: s[0], 2: s[1], 3: s[2], 4: s[3], 5: s[4], 6: s[5], 7: s[6], 8: s[7], 9: s[8] };
-    })());
+    // Find the new nine-letter word
+    const word = dict9[Math.floor(Math.random() * dict9.length)];
 
     const letters = {};
 
-    // Iterate over letters of nine-letter word in this.letters
-    for (let i = 1; i < 10; i += 1) {
-      if (typeof (letters[this.letters[i]]) === 'undefined') {
-        letters[this.letters[i]] = 0;
+    // Iterate over letters of nine-letter word
+    for (let i = 0; i < 9; i += 1) {
+      if (typeof (letters[word[i]]) === 'undefined') {
+        letters[word[i]] = 0;
       }
-      letters[this.letters[i]] += 1;
+      letters[word[i]] += 1;
     }
 
+    const start = new Date().getTime();
     console.log(`Letters are ${Object.keys(letters)}`);
 
     // Filter dictionary to only words that contain the center letter
     const words = [];
-    const dicts = [dict4];
+    const dicts = [dict4, dict5, dict6, dict7, dict8, dict9];
 
     // Iterate over dictionary objects on dict[i]
     for (let i = 0; i < dicts.length; i += 1) {
       // Iterate over words in each dictionary on dicts[i][j]
       for (let j = 1; j < dicts[i].length; j += 1) {
         // Break the word apart into letters
-        const l = dicts[i][j].split('');
-        const matches = {};
+        const l = dicts[i][j].split('').sort();
+
+        // Match helps deal with repeat letters
+        let match = ['', 0];
+        let total = 0;
         // Iterate over the letters of our word on l[k]
         for (let k = 0; k < l.length; k += 1) {
           if (typeof (letters[l[k]]) !== 'undefined') {
-            if (typeof (matches[l[k]]) === 'undefined') {
-              matches[l[k]] = 0;
-            }
-            if (matches[l[k]] < letters[l[k]]) {
-              matches[l[k]] += 1;
+            if (match[0] === l[k]) { // This match is a repeated letter
+              if (match[1] < letters[l[k]]) { // Only match if this many letters are in the word
+                total += 1;
+                match[1] += 1;
+              }
+            } else { // This match is not repeated (yet)
+              total += 1;
+              match = [l[k], 1];
             }
           }
         }
-        // Only proceed if the word contains the center letter
-        if (typeof (matches[this.letters[5]]) !== 'undefined') {
-          if (Object.values(matches).reduce((a, b) => a + b) === dicts[i][j].length) {
-            words.push(dicts[i][j]);
-          }
+        // Only proceed if all letters are present
+        if (total === dicts[i][j].length) {
+          words.push(dicts[i][j]);
         }
       }
     }
 
-    console.log(`List of words contains ${words.length} items.`);
+    // See how many valid words we'd have for each different middle letter
+    const lengths = Object.assign({}, ...word.split('').map(l => ({ [l]: 0 })));
+    console.log(lengths);
+    const lengthItems = Object.keys(lengths);
+
+    for (let i = 0; i < lengthItems.length; i += 1) {
+      const matches = words.filter(w =>
+        (w.indexOf(lengthItems[i]) !== -1),
+      );
+      lengths[lengthItems[i]] = matches.length;
+    }
+
+    // Log the result
+    for (let i = 0; i < lengthItems.length; i += 1) {
+      console.log(`${lengthItems[i]} has ${lengths[lengthItems[i]]} matches.`);
+    }
+
+    // Generate a scrambled nine-letter word:
+    Object.assign(this.letters, (typeof (options.letters) === 'object') ? options.letters : (() => {
+      const s = shuffle(word).split('');
+      return { 1: s[0], 2: s[1], 3: s[2], 4: s[3], 5: s[4], 6: s[5], 7: s[6], 8: s[7], 9: s[8] };
+    })());
+    
+    const end = new Date().getTime();
+    console.log(`Word generation took ${end - start} milliseconds.`);
 
     // Set the other variables
     this.timer = (typeof (options.timer) === 'number') ? options.timer : -1;
