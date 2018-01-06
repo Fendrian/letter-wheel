76e729b4d551e8f0a1dca6117ba68403ee8bfd8a
Wolf Hatch <wolf@mobi2go.com>
2018-01-06 14:53:21 +1300

    Expanding tests

---
+++ src/__tests__/index.test.js
@@ -0,0 +1,24 @@
+import { AppRegistry } from 'react-native';
+import targetWords from '../Entry';
+import '../index';
+
+jest.mock('react-native', () => (
+  {
+    AppRegistry: {
+      registerComponent: jest.fn(),
+    },
+  }
+));
+jest.mock('../Entry', () => {});
+
+describe('Main index', () => {
+  it('Registers targetWords', () => {
+    const register = AppRegistry.registerComponent;
+    expect(register).toHaveBeenCalledTimes(1);
+    expect(register).toHaveBeenCalledWith(
+      'targetwords',
+      expect.any(Function),
+    );
+    expect(register.mock.calls[0][1]()).toEqual(targetWords);
+  });
+});
